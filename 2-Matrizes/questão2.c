1067d0b79de3003d8cab7328bdd4a3ee465a04e1
Vinicius Maia <89935012+ViiniciusMaiia80@users.noreply.github.com>
2025-05-25 23:05:42 -0400

    questão 2 - matrizes - REFATORADA COM FUNÇÕES
    Toda implementação no metodo int main(); foi remanejadas em funções:
    lerMatrizes();
    exibirMatriz();
    maiorQue10();

--- 2-Matrizes/questão2.c
+++ 2-Matrizes/questão2.c
@@ -1,6 +1,80 @@
 /*02 - Leia uma matriz 4 x 4, conte e escreva quantos valores maiores que 10 ela 
 possui.*/
 #include <stdio.h>
+#include <stdlib.h>
+#define X 4
+int contador=0;
+void lerMatrizes(float m1[X][X]){
+    for(int i=0; i<X;i++){
+        for(int j=0; j<X; j++){
+            m1[i][j] = rand() % 30;
+        }
+    }
+}
+void exibirMatriz(float m1[X][X]){
+    for(int i=0; i<X;i++){
+        for(int j =0;j<X;j++){
+            printf("%.f ", m1[i][j]);
+        }
+        printf("\n");
+    }
+}
+void maiorQue10(float m1[X][X]){
+    for(int i=0; i<X; i++){
+        for(int j=0;j<X;j++){
+            if(m1[i][j]>10){
+                contador++;
+            }
+        }
+    }
+    printf("Elementos maiores que 10 na matriz: %i", contador);
+}
+int main(){
+    float m1[X][X];
+    lerMatrizes(m1);
+    exibirMatriz(m1);
+    maiorQue10(m1);
+    return 0;
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <stdio.h>
 
 #define TAMANHO 4
 
@@ -27,3 +101,4 @@ int main() {
 
     return 0;
 }
+*/
