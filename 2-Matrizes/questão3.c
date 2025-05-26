ec7766d5dbebd2345c88c539eb9b9a6cd052cc58
Vinicius Maia <89935012+ViiniciusMaiia80@users.noreply.github.com>
2025-05-26 11:30:55 -0400

    questões 3 e 4 refatoradas utilizando funções
    preencherDiagonalcom1(m);
        exibirMatriz(m);

--- 2-Matrizes/questão3.c
+++ 2-Matrizes/questão3.c
@@ -1,28 +1,35 @@
-/*03 - Declare em C matriz 5 x 5. Preencha com 1 a diagonal principal e com 0 os demais elementos. Escreva ao final a matriz obtida.*/
+/*03 - Declare em C matriz 5 x 5. Preencha com 1 a diagonal principal 
+e com 0 os demais elementos. Escreva ao final a matriz obtida.*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#define X 5
 
-#define TAMANHO 5
-
-int main() {
-    int matriz[TAMANHO][TAMANHO];
-
-    
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
-            if (i == j) {
-                matriz[i][j] = 1;  
-            } else {
-                matriz[i][j] = 0;  
+void preencherDiagonalcom1(int m[X][X]){
+    for(int i=0;i<X;i++){
+        for(int j=0;j<X;j++){
+            if(i==j){
+                m[i][j]= 1;
+            }else{
+                m[i][j]=0;
             }
         }
     }
-    printf("Matriz 5x5:\n");
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
-            printf("%d ", matriz[i][j]);
+}
+void exibirMatriz(int m[X][X]){
+    printf("matriz com diagonal 1: \n");
+    for(int i=0;i<X;i++){
+        for(int j=0; j<X;j++){
+            printf("%i ", m[i][j]);
         }
         printf("\n");
     }
+}
+
+int main(){
+    int m[X][X];
+    preencherDiagonalcom1(m);
+    exibirMatriz(m);
 
     return 0;
 }
--- 2-Matrizes/questão4.c
+++ 2-Matrizes/questão4.c
@@ -1,48 +1,41 @@
 /*04 – Leia uma matriz 5x10 que se refere às repostas de 10 questões de múltipla 
 escolha de 5 alunos. Leia também um vetor de 10 posições contendo o gabarito de 
 respostas que podem ser a, b, c ou d. Seu programa deve comparar as respostas de 
-cada candidato como gabarito e emitir um vetor denominado resultado, contendo 
+cada candidato com o gabarito e emitir um vetor denominado resultado, contendo 
 a pontuação de cada aluno.*/
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
 
-#define ALUNOS 5
-#define QUESTOES 10
-
-int main() {
-    char respostas[ALUNOS][QUESTOES]; 
-    char gabarito[QUESTOES]; 
-    int resultado[ALUNOS] = {0};
-
-    
-    printf("Digite as respostas dos %d alunos (a, b, c ou d):\n", ALUNOS);
-    for (int i = 0; i < ALUNOS; i++) {
-        printf("Aluno %d:\n", i + 1);
-        for (int j = 0; j < QUESTOES; j++) {
-            printf("Questao %d: ", j + 1);
-            scanf(" %c", &respostas[i][j]);
+int main(){
+    int min='a', max='d', gabarito[10], resultado[5], repostaCerta=0;
+    char m[5][10];
+    for(int i=0;i<5;i++){
+        printf("Resposta aluno %i: ", i);
+        for(int j=0;j<10;j++){
+            m[i][j]= (rand() % (max - min + 1)) + min;
+            printf("%c ", m[i][j]);
         }
+        printf("\n");
     }
 
-    
-    printf("\nDigite o gabarito das %d questoes (a, b, c ou d):\n", QUESTOES);
-    for (int i = 0; i < QUESTOES; i++) {
-        printf("Questao %d: ", i + 1);
-        scanf(" %c", &gabarito[i]);
+    printf("Gabarito:         ");
+    for(int i=0;i<10;i++){
+        gabarito[i] = (rand() % (max-min+1))+min;
+        printf("%c ", gabarito[i]);
     }
 
-    
-    for (int i = 0; i < ALUNOS; i++) {
-        for (int j = 0; j < QUESTOES; j++) {
-            if (respostas[i][j] == gabarito[j]) {
-                resultado[i]++; 
+    for(int i=0; i<5; i++){
+        for(int j=0;j<10;j++){
+            if(m[i][j]==gabarito[i]){
+                repostaCerta++;
             }
         }
     }
 
-    printf("\nPontuacao dos alunos:\n");
-    for (int i = 0; i < ALUNOS; i++) {
-        printf("Aluno %d: %d acertos\n", i + 1, resultado[i]);
+    for(int i=0; i<5;i++){
+        resultado[i]= repostaCerta;
+        printf("\nA nota do aluno %i eh: %i ", i+1, resultado[i]);
     }
-
     return 0;
+
 }
